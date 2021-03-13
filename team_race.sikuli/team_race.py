c993c93dd9f6c8488def46f32f8b4360f7620c89
dairappa <dairappa@gmail.com>
2021-03-14 05:43:39 +0900

    fix missing click while multiple runs

--- team_race.sikuli/team_race.py
+++ team_race.sikuli/team_race.py
@@ -1,9 +1,10 @@
 def team_race():
-    click(Pattern("1615497174754.png").targetOffset(206,-164))
-
     sleep(3)
-
-    click("1615497277695.png")
+    click(Pattern("1615497174754.png").targetOffset(206,-164))
+    
+    sleep(2)
+    
+    click(Pattern("1615497174754.png").targetOffset(236,3))
 
     sleep(2)
 
@@ -23,20 +24,16 @@ def team_race():
 
 
     # TODO: ハイスコア更新対応
-    isHighscore = has(Pattern("1615527252775.png").similar(0.50))
+    isHighscore = has(Pattern("1615527252775.png").similar(0.50), 5)
 
     if isHighscore:
         click(Pattern("1615527252775.png").similar(0.50))
-
-    
-    wait("1615497350224.png", 10)
-    click("1615527387555.png")
 
 
 
     # TODO:　アイテム拾ったとき対応
 
-    hasItem = has(Pattern("1615526489319.png").similar(0.50), 10)
+    hasItem = has(Pattern("1615526489319.png").similar(0.50), 5)
 
     if hasItem:
         click("1615526540633.png")
@@ -51,7 +48,7 @@ def team_race():
 
 
     # TODO: ストーリー開放
-    newStory = has("1615527447356.png", 10)
+    newStory = has("1615527447356.png", 5)
     if newStory:
         click("1615527483994.png")
 
@@ -61,7 +58,11 @@ def team_race():
 
     click("1615497071387.png")
 
-    sleep(3)
+    noRP = has("1615667482724.png", 5)
+
+    if noRP:
+        click(Pattern("1615667573486.png").targetOffset(-107,172))
+        return False
 
     return True
 
@@ -71,10 +72,8 @@ click("1615496030640.png")
 
 click("1615496064507.png")
 click(Pattern("1615496097944.png").targetOffset(-12,-136))
-sleep(3)
-click(Pattern("1615497174754.png").targetOffset(206,-164))
 
-sleep(3)
+
 
 succeeded = True
 
