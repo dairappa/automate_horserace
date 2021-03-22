1c6afe800a86678ab2636698de88a16ebc4b29e3
dairappa <dairappa@gmail.com>
2021-03-22 13:49:42 +0900

    fix bakushin

--- bakushin.sikuli/bakushin.py
+++ bakushin.sikuli/bakushin.py
@@ -1,56 +1,106 @@
 reg = Region(0,0,601,1111)
 
-def waitClick(image, waitSecond = 5):
+def waitClick(image, waitSecond = 10):
     reg.wait(image, waitSecond)
     reg.click()
 
+def runRace():
+    reg.click("1616365555818.png")
+    wait(2)
+    
+    reg.click("1616365555818.png")
+    waitClick("1616365717674.png")
+    wait(3)
+    reg.click(Pattern("1615955389096.png").targetOffset(268,627))
+    
+    waitClick("1616365790564.png")
+    waitClick("1616365816547.png")
+
+
 
 def loop():
+    if reg.has(Pattern("1616388322750.png").similar(0.90)):
+        reg.click()
+        return True
     
-    if not reg.has("1616053302005.png") and reg.has(Pattern("1615978315697.png").similar(0.85)):
+    if not reg.has(Pattern("1616053302005.png").similar(0.90)) and reg.has(Pattern("1615978315697.png").similar(0.85)) and not reg.has(Pattern("1616368003968.png").similar(0.95)):
         reg.click()
 
-        if reg.has("1615978551852.png"):
+        if reg.has("1615978551852.png", 5):
             reg.click("1615978573903.png")
             wait(2)
             # returnせずに次の判定にうつる
         else:
-            return True
+            wait(2)
+            if reg.has(Pattern("1616386784155.png").similar(0.90)): 
+                reg.click()
+                runRace()
+                return True
+                
+            elif reg.has("1616386799656.png") : 
+                reg.click()
+                runRace()
+                return True
+            
+            elif reg.has(Pattern("1616365555818.png").targetOffset(157,-169)):
+                reg.click(Pattern("1616365555818.png").targetOffset(157,-169))
+                wait(2)
+                if reg.has(Pattern("1616386784155.png").similar(0.90)): 
+                    reg.click()
+                    runRace()
+                    return True
+                elif reg.has("1616386799656.png") : 
+                    reg.click()
+                    runRace()
+                    return True
+                else:
+                   reg.click("1616384941178.png")
+                   #　returnせずに次の判定へ
+                   wait(3)
+
+            else:
+                reg.click("1616384941178.png")
+                # returnせずに次の判定へ
+                wait(3)
+
+
+
 
     
     if reg.has("1615955063604.png"):
        reg.click()
 
+       if reg.has("1616387184684.png", 3):
+          reg.click("1616387228621.png") 
+          wait(3)
 
-       waitClick("1615955176409.png")
-
-       waitClick("1615955176409.png")
-       waitClick("1615955359286.png")
-       reg.wait(3)
-       reg.click(Pattern("1615955389096.png").targetOffset(-344,250))
-
-       waitClick("1615955437110.png")
-       waitClick("1615955468286.png")
+       runRace()
 
        waitClick("1615956221725.png")
+       wait(2)
+       waitClick("1615956221725.png")
+       
        return True
 
-    if has("1615928744521.png"):
-        return False
-    
-    if has(Pattern("1615929011233.png").similar(0.90)):
-        click("1615928174362.png")
-        wait(1)
-        click("1615928199593.png")
+    if reg.has("1615928744521.png") and reg.has("1616367046236.png"):
+        reg.click("1616367046236.png")
+        
+        waitClick("1615928199593.png")
         return True
-
-
-
-    #if reg.has(Pattern("1615928271653.png").similar(0.90)):
-    #    click()
-    #    waitClick("1615928199593.png")
-    #    return
-
+        
+    if reg.has(Pattern("1616366143315.png").similar(0.90)):
+        reg.click()
+        if reg.has("1616366183098.png", 5):
+            waitClick("1615928199593.png")
+            return True
+        else:
+            # ポップアップが出なかった場合、誤クリックなので流す
+            wait(1)
+    
+    if reg.has(Pattern("1615929011233.png").similar(0.90)) and reg.has("1615928174362.png"):
+        reg.click("1615928174362.png")
+        waitClick("1615928199593.png")
+        return True
 
 
 
